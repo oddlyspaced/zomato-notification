15af9b0e59d53d33b52a29cac609d48077ed3b12
oddlyspaced <oddlyspaced1@gmail.com>
2024-06-09 15:02:43 +0530

    fix: hilt dependency version for kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
