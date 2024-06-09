7571b1ae51f7ab47193158f49c4c8919f2298b07
oddlyspaced <oddlyspaced1@gmail.com>
2024-06-09 16:59:07 +0530

    chore: setup hilt module for retrofit

---
+++ app/src/main/java/com/oddlyspaced/zomato/notification/di/ApiModule.kt
@@ -0,0 +1,39 @@
+package com.oddlyspaced.zomato.notification.di
+
+import com.oddlyspaced.zomato.notification.api.Api
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Singleton
+
+/**
+ * @author : hardik
+ * @created : 09/06/24, Sunday
+ **/
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideApi(): Api {
+        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client: OkHttpClient = OkHttpClient.Builder().apply {
+            addInterceptor(interceptor)
+        }.build()
+        val retrofit = Retrofit.Builder().baseUrl("https://api.zomato.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(Api::class.java)
+    }
+}
